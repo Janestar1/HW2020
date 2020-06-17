bf0938046dab3ca7348f2759b514b175ab227da0
Ying Zhen <58847845+Janestar1@users.noreply.github.com>
2020-06-17 10:49:31 +0800

    delete

--- scripts.js
+++
@@ -1,9 +0,0 @@
-let tasks = [];
-function renderEditor()
-    let inputEl = document.querySelector("#default-todo-panel.todo-editor > input")
-    inputEl.onchange = (e) =>{
-        console.log("text,",e.target.value);
-        console.log("input change:",e);
-    };
-
-let addEl = document.querySelector("#default-")
